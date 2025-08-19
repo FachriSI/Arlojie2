a0ab5f27f2f300332b09b457066df5d6ccca8785
Indah Syahfitri <146148856+ethereins@users.noreply.github.com>
2025-08-19 19:07:44 +0700

    Delete ArlojieBE directory

--- ArlojieBE/routes/dashboardRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authMiddleware = require('../middlewares/authMiddleware');
-
-router.get('/dashboard', authMiddleware, (req, res) => {
-  res.json({ message: 'Ini data dashboard, hanya bisa diakses jika login!' });
-});
-
-module.exports = router;
