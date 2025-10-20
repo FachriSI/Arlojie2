c775a3f9baeff934c24d614ece870657c28a6c9f
Indah Syahfitri <146148856+ethereins@users.noreply.github.com>
2025-10-20 18:39:41 +0700

    Delete ArlojieBE/routes/adminAnalyticsRoutes.js

--- ArlojieBE/routes/adminAnalyticsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const adminAnalyticsController = require('../controllers/adminAnalyticsController');
-const { verifyToken, isAdmin } = require('../middlewares/authMiddleware');
-
-router.get('/analytics', verifyToken, isAdmin, adminAnalyticsController.getAnalytics);
-
-module.exports = router;
